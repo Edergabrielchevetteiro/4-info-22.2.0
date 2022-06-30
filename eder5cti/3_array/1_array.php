d95c9c1aa94cae99946a9504ce1545c2051460e6
Edergabrielchevetteiro <108496907+Edergabrielchevetteiro@users.noreply.github.com>
2022-06-30 20:53:44 -0300

    Add files via upload
    envio introduçao 30/06/22

---
+++ eder5cti/3_array/1_array.php
@@ -0,0 +1,38 @@
+<?php
+	$cores1 = array ('vermelho', 'azul', 'verde', 'amarelo');
+
+	var_dump($cores1);
+	echo '<br>';
+	echo $cores1[0];
+	echo '<br>';
+	echo $cores1[1];
+	echo '<hr>';
+	
+	$frutas = array (0=>'jabuticaba',5=>'carambola', 2 =>'tomate');
+	var_dump($frutas);
+	echo '<br>';
+	echo $frutas[0];
+	echo '<br>';
+	echo $frutas[5];
+	echo '<br>';
+	echo $frutas[2];
+	echo '<hr>';
+	
+	
+	$carros = ['diabo verde','kombis','del rey','belina','mareão'];
+  var_dump($carros);
+	echo '<br>';
+	echo $carros[0];
+	echo '<br>';
+	echo $carros[1];
+	echo '<br>';
+	echo $carros[2];
+	echo '<br>';
+	echo $carros[3];
+	echo '<br>';
+	echo $carros[4];
+	
+	echo '<hr>';
+
+s
+?>
